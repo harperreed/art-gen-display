7e324bb688fd59e70e470b10d1911f2603c49d58
Harper Reed <harper@nata2.org>
2023-11-15 22:46:21 -0600

    a better prompt generator

--- app.js
+++ app.js
@@ -31,18 +31,18 @@ io.on('connection', (socket) => {
 
 function generatePrompt() {
   const types = ["photo", "painting", "vector image", "digital art"];
-  const keywords = ["panda", "cityscape", "rain", "night"];
-  const styles = ["vibrant", "surreal", "photorealistic", "painting-like"];
-  const artistStyles = ["picasso", "hiroshi nagai", "murakami", "ansel adams", "maplethorpe"];
+  const feelings = ["lonely", "excited", "sad", "depressed"];
+  const keywords = ["cyborg", "fireworks", "forest", "cityscape", "rain", "night", "snow", "wolf", "80s hifi bar"];
+  const styles = ["vibrant", "surreal", "photorealistic", "painting-like", "collage"];
+  const artistStyles = ["Jean-Michel Basquiat", "Marlene Dumas", "keith haring", "picasso", "Yayoi Kusama", "hiroshi nagai", "murakami", "ansel adams", "robert maplethorpe", "Hiroshi Sugimoto"];
 
   const randomType = types[Math.floor(Math.random() * types.length)];
+  const randomFeeling = feelings[Math.floor(Math.random() * feelings.length)];
   const randomKeyword = keywords[Math.floor(Math.random() * keywords.length)];
   const randomStyle = styles[Math.floor(Math.random() * styles.length)];
   const randomArtistStyle = artistStyles[Math.floor(Math.random() * artistStyles.length)];
-  
-  // Construct the prompt
-  // You can customize the prompt pattern as needed
-  return `a black and white ${randomType} of ${randomKeyword} in ${randomStyle} style made by ${randomArtistStyle} for display in a museum `;
+
+  return `a black and white ${randomType} of a ${randomFeeling} ${randomKeyword} in ${randomStyle} style made by ${randomArtistStyle} for display in a museum `;
 }
 
 
